501b2e5dcddca7ee31169ca566aa76837e2ce932
MarcusviniciusLsantos <marcusvinici25@gmail.com>
2019-10-17 16:55:22 -0300

    Updates

---
+++ precache-manifest.292bcce760c842d73eb0c597fd0a8755.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbcb7677222cd50829530f8ca8b4c118",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "16ed97fc6ef44eb40e3d",
+    "url": "/my-portfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "205077743e1bc62837b9",
+    "url": "/my-portfolio/static/js/2.c67c5a6f.chunk.js"
+  },
+  {
+    "revision": "16ed97fc6ef44eb40e3d",
+    "url": "/my-portfolio/static/js/main.2a135a89.chunk.js"
+  },
+  {
+    "revision": "39485d276cb6a495935f",
+    "url": "/my-portfolio/static/js/runtime-main.1cf6ada0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-portfolio/static/media/logo.25bf045c.svg"
+  }
+]);
